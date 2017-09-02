b588e9e589175fda4114f4b5d0923a6c9a0ce44d
Nikhil Prabhu <nikhill.prabhu@gmail.com>
2017-09-02 17:35:45 +0530

    plots columns vs dict length

--- main/plot.py
+++ main/plot.py
@@ -3,19 +3,23 @@ import pylab
 
 from Constants import directories
 from files import getJSONObject
+from helper import printMatrix
 
 numSongs = getJSONObject(directories.NUM_SONGS_TRAIN)
 genreFreqsTrain = getJSONObject(directories.GENRE_FREQS_TRAIN)
+results = getJSONObject(directories.RESULTS)
 
+printMatrix(results, directories.MATRIX_OUTPUT)
 
-#print map(lambda x:x[0], sorted(numSongs.items()))
-#print map(lambda x:x[1], sorted(numSongs.items()))
-#print map(lambda x:x[0], sorted([(g,len(genreFreqsTrain[g])) for g in genreFreqsTrain.keys()]))
-#print map(lambda x:x[1], sorted([(g,len(genreFreqsTrain[g])) for g in genreFreqsTrain.keys()]))
-
-songs = map(lambda x:x[1], sorted(numSongs.items()))
+columns = map(lambda x:x[1], sorted([(g,sum([results[genre][g] for genre in results.keys()])) for g in results.keys()]))
 data = map(lambda x:x[1], sorted([(g,len(genreFreqsTrain[g])) for g in genreFreqsTrain.keys()]))
-plt.plot(songs, data, 'ro')
-plt.xlabel('songs')
-plt.ylabel('data')
+
+matrixSum = sum(columns)
+
+print columns
+print data
+
+plt.plot(data, columns, 'ro')
+plt.xlabel('amount of data')
+plt.ylabel('number of times classified')
 plt.show()
