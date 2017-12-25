2923ecd42fad5d07edfa2b13bab1ff609584d012
Nikhil Prabhu <nikhill.prabhu@gmail.com>
2017-12-25 19:54:09 +0530

    flips iteration in getFreqsGenre from song->genres to genre->songs

--- main/lib/learn.py
+++ main/lib/learn.py
@@ -9,7 +9,8 @@ from core.main.lib.sentiment import getSentimentVector, getSentimentCount
 from core.main.lib.spanish import countLangWords
 from core.main.Util.prob import classifyGenreSong
 from core.main.Util.timing import timer
-from core.main.lib.ngrams import getFrequenciesGenre
+
+
 def createFeatureMatrix(spanishWords, germanWords, frenchWords, path, lyricStore, bigramTotality, trigramTotality, bigramSongFreqs, trigramSongFreqs):
 	#print path
 	#print os.getcwd()
@@ -46,7 +47,7 @@ def createFeatureMatrix(spanishWords, germanWords, frenchWords, path, lyricStore
 			numSpanish = countLangWords(words, spanishWords)
 			numGerman = countLangWords(words, germanWords)
 			numFrench = countLangWords(words, frenchWords)
-			freqBigrams,freqTrigrams = bigramSongFreqs[genre][song],trigramSongFreqs[genre][song]
+			freqBigrams,freqTrigrams = bigramSongFreqs[genre][song], trigramSongFreqs[genre][song]
 			#row = [length, lines, avgLineLength, avgLen, titleLen, numSpanish, numGerman, numFrench]
 			row = [length, avgLen, titleLen, numSpanish, numGerman, numFrench, freqBigrams, freqTrigrams]
 			row += sentVec
@@ -54,50 +55,6 @@ def createFeatureMatrix(spanishWords, germanWords, frenchWords, path, lyricStore
 			labels.append(values.GEN_NUMBER[genre])
 			#print song, len(words)
 			#print words
-
-	return np.array(features, dtype=float), np.array(labels, dtype=float)
-
-
-def _createFeatureMatrix(spanishWords, germanWords, frenchWords, path):
-	#print path
-	#print os.getcwd()
-	#print os.path.abspath(path)
-	#print os.listdir(path)
-	features, labels = [], []
-
-	for genre in GENRES:
-		print 'GENRE:', genre
-		for dirpath, dirnames, files in os.walk(getGenrePath(path, genre)):
-			for file in files:
-				song = open(dirpath + "/" + file)
-				words = []
-				lineLengths = []
-				lines = 0
-				for line in song.readlines():
-					words += line.split(' ')
-					lineLengths.append(len(words))
-					lines += 1
-
-				length = len(words)
-				if length != 0:
-					avgLen = sum([len(word) for word in words])/length
-				else:
-					avgLen = 0
-				if lines != 0:
-					avgLineLength = sum([lineLen for lineLen in lineLengths])/lines
-				else:
-					avgLineLength = 0
-				titleLen = len(file.split(" "))
-				sentVec = getSentimentVector(words)
-				numSpanish = countLangWords(words, spanishWords)
-				numGerman = countLangWords(words, germanWords)
-				numFrench = countLangWords(words, frenchWords)
-				row = [length, lines, avgLineLength, avgLen, titleLen, numSpanish, numGerman, numFrench]
-				row += sentVec
-				features.append(row)
-				labels.append(values.GEN_NUMBER[genre])
-				print file, len(words)
-				print words
 
 	return np.array(features, dtype=float), np.array(labels, dtype=float)
 
--- main/lib/ngrams.py
+++ main/lib/ngrams.py
@@ -24,7 +24,9 @@ def saveNgramDistsGenre(lyricStore, bigramStoreTotality, trigramStoreTotality, g
 	saveJSONObject(bigramDist, bigramStoreTotality[genre])
 	#saveJSONObject(trigramDist, trigramStoreTotality[genre])
 
-def getFrequenciesGenre(words,bigramStoreTotality,trigramStoreTotality):
+
+'''
+def _getFrequenciesGenre(words,bigramStoreTotality,trigramStoreTotality):
 	bigrams = list(ngrams(words,2))
 	trigrams = list(ngrams(words,3))
 
@@ -43,36 +45,53 @@ def getFrequenciesGenre(words,bigramStoreTotality,trigramStoreTotality):
 
 	return frequencyVecBigrams,frequencyVecTrigrams
 
-'''
-def getCountsEachSong(lyricStore, bigramStoreFreqs, trigramStoreFreqs):
-	for genre in GENRES:
-		print 'GENRE:', genre
-		lyrics = getJSONObjectCodec(lyricStore[genre])
-		bigramCounts = {}
-		trigramCounts = {}
-		for song, words in lyrics.items():
-			bigrams = FreqDist(list(ngrams(words, 2)))
-			trigrams = FreqDist(list(ngrams(words, 3)))
-			bigramCounts[song] = bigrams
-			trigramCounts[song] = trigrams
-		print(bigramCounts)
-		saveJSONObject(bigramCounts, bigramStoreFreqs[genre])
-		saveJSONObject(trigramCounts, trigramStoreFreqs[genre])
-'''
 
-def getFreqsByGenre(lyricsStore,bigramStoreTotality,trigramStoreTotality,bigramFreqsStore,trigramFreqsStore):
+def _getFreqsByGenre(lyricsStore,bigramStoreTotality,trigramStoreTotality,bigramFreqsStore,trigramFreqsStore):
 	for genre in GENRES:
 		songCountsBigram = {}
 		songCountsTrigram = {}
 		lyrics = getJSONObject(lyricsStore[genre])
 		print(len(lyrics.items()))
+
 		for song, words in lyrics.items():
-			freqBigrams, freqTrigrams = getFrequenciesGenre(words, bigramStoreTotality, trigramStoreTotality)
+			freqBigrams, freqTrigrams = _getFrequenciesGenre(words, bigramStoreTotality, trigramStoreTotality)
 			songCountsBigram[song] = freqBigrams
 			songCountsTrigram[song] = freqTrigrams
 		print(songCountsBigram,songCountsTrigram)
 		saveJSONObject(songCountsBigram,bigramFreqsStore[genre])
 		saveJSONObject(songCountsTrigram,trigramFreqsStore[genre])
+'''
 
 
+def getFreqsByGenre(lyricsStore, bigramStoreTotality, trigramStoreTotality, bigramFreqsStore, trigramFreqsStore):
+	for genre in GENRES:
+		bigramFrequencies = {}
+		trigramFrequencies = {}
+		lyrics = getJSONObject(lyricsStore[genre])
+		print(len(lyrics.items()))
 
+		for genre in GENRES:
+			bigramStoreTotalityObj = getJSONObject(bigramStoreTotality[genre])
+			trigramStoreTotalityObj = getJSONObject(trigramStoreTotality[genre])
+
+			for song, words in lyrics.items():
+				if song not in bigramFrequencies:
+					bigramFrequencies[song] = [0] * len(GENRES)
+
+				if song not in trigramFrequencies:
+					trigramFrequencies[song] = [0] * len(GENRES)
+
+				bigrams = list(ngrams(words, 2))
+				trigrams = list(ngrams(words, 3))
+
+				for bigram in bigrams:
+					if bigram in bigramStoreTotalityObj:
+						bigramFrequencies[song][GENRES.index(genre)] += bigramStoreTotalityObj[bigram]
+
+				for trigram in trigrams:
+					if trigram in trigramStoreTotalityObj:
+						trigramFrequencies[song][GENRES.index(genre)] += trigramStoreTotalityObj[trigram]
+
+		print(bigramFrequencies, trigramFrequencies)
+		saveJSONObject(bigramFrequencies, bigramFreqsStore[genre])
+		saveJSONObject(trigramFrequencies, trigramFreqsStore[genre])
