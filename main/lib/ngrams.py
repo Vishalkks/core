95040beaa079d4e76a6a4b00469f6c12c8b76f1e
Nikhil Prabhu <nikhill.prabhu@gmail.com>
2017-12-25 20:32:11 +0530

    Replaces saveNgramDists and saveNgramDistsGenre with saveNGramDists

--- main/lib/ngrams.py
+++ main/lib/ngrams.py
@@ -21,6 +21,44 @@ def saveNGramDists(lyricStore, bigramStoreTotality, trigramStoreTotality):
 		saveJSONObject(trigramDist, trigramStoreTotality[genre])
 
 
+'''
+def _getFrequenciesGenre(words,bigramStoreTotality,trigramStoreTotality):
+	bigrams = list(ngrams(words,2))
+	trigrams = list(ngrams(words,3))
+
+	frequencyVecBigrams = [0]*len(GENRES)
+	frequencyVecTrigrams = [0] * len(GENRES)
+	for genre in GENRES:
+		bigramStoreTotalityObj = getJSONObject(bigramStoreTotality[genre])
+		for bigram in bigrams:
+			if bigram in bigramStoreTotalityObj:
+				frequencyVecBigrams[GENRES.index(genre)] += bigramStoreTotalityObj[bigram]
+
+		trigramStoreTotalityObj = getJSONObject(trigramStoreTotality[genre])
+		for trigram in trigrams:
+			if trigram in trigramStoreTotalityObj:
+				frequencyVecTrigrams[GENRES.index(genre)] += trigramStoreTotalityObj[trigram]
+
+	return frequencyVecBigrams,frequencyVecTrigrams
+
+
+def _getFreqsByGenre(lyricsStore,bigramStoreTotality,trigramStoreTotality,bigramFreqsStore,trigramFreqsStore):
+	for genre in GENRES:
+		songCountsBigram = {}
+		songCountsTrigram = {}
+		lyrics = getJSONObject(lyricsStore[genre])
+		print(len(lyrics.items()))
+
+		for song, words in lyrics.items():
+			freqBigrams, freqTrigrams = _getFrequenciesGenre(words, bigramStoreTotality, trigramStoreTotality)
+			songCountsBigram[song] = freqBigrams
+			songCountsTrigram[song] = freqTrigrams
+		print(songCountsBigram,songCountsTrigram)
+		saveJSONObject(songCountsBigram,bigramFreqsStore[genre])
+		saveJSONObject(songCountsTrigram,trigramFreqsStore[genre])
+'''
+
+
 def getFreqsByGenre(lyricsStore, bigramStoreTotality, trigramStoreTotality, bigramFreqsStore, trigramFreqsStore):
 	for genre in GENRES:
 		bigramFrequencies = {}
